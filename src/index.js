ef6495461b1b7b5625f59a5b0eb6ce9c9ee64f6c
Alex <alexey.emshanov@39parrot.net>
2015-09-12 17:30:19 +0400

    add layots

--- src/index.js
+++ src/index.js
@@ -1,38 +1,86 @@
 'use strict';
 
-// Famous dependencies
-var DOMElement = require('famous/dom-renderables/DOMElement');
-var FamousEngine = require('famous/core/FamousEngine');
+var famous = require('famous');
+var avatar = require('./avatar');
+var avatarList = require('./avatarList');
+var DOMElement = famous.domRenderables.DOMElement;
+var FamousEngine = famous.core.FamousEngine;
+var GestureHandler = famous.components.GestureHandler;
+var Transitionable = require('famous/transitions/Transitionable');
+var Position = famous.components.Position;
+var Transitionable = require('famous/transitions/Transitionable');
+var Align = famous.components.Align;
 
-// Boilerplate code to make your life easier
-FamousEngine.init();
+var scene= FamousEngine.createScene();
 
-// Initialize with a scene; then, add a 'node' to the scene root
-var logo = FamousEngine.createScene().addChild();
+var mainNode = scene.addChild();
+mainNode
+    .setSizeMode('relative', 'relative');
 
-// Create an [image] DOM element providing the logo 'node' with the 'src' path
-new DOMElement(logo, { tagName: 'img' })
-    .setAttribute('src', './images/famous_logo.png');
+mainNode.el = new DOMElement(mainNode, {
+    properties: {
+        'background-color': '#CCCEC7'
 
-// Chainable API
-logo
-    // Set size mode to 'absolute' to use absolute pixel values: (width 250px, height 250px)
-    .setSizeMode('absolute', 'absolute', 'absolute')
-    .setAbsoluteSize(250, 250)
-    // Center the 'node' to the parent (the screen, in this instance)
-    .setAlign(0.5, 0.5)
-    // Set the translational origin to the center of the 'node'
-    .setMountPoint(0.5, 0.5)
-    // Set the rotational origin to the center of the 'node'
-    .setOrigin(0.5, 0.5);
-
-// Add a spinner component to the logo 'node' that is called, every frame
-var spinner = logo.addComponent({
-    onUpdate: function(time) {
-        logo.setRotation(0, time / 1000, 0);
-        logo.requestUpdateOnNextTick(spinner);
     }
+})
+
+var item1 = mainNode.addChild();
+item1
+    .setProportionalSize(0.8, 0.1)
+    .setMountPoint(0.5, 0.5)
+    .setAlign(0.5, 0.1);
+
+item1.el = new DOMElement(item1, {
+    properties: {
+        'background-color': '#E3F5BD',
+        'border': '5px solid',
+        'border-color': 'whitesmoke',
+        'border-radius': '20px',
+        'box-shadow': '7px 7px 10px #888888'
+    }
+})
+
+var draggingNode = scene.addChild();
+draggingNode
+    .setProportionalSize(1, 1)
+    .setAlign(0.95, 0);
+
+draggingNode.el = new DOMElement(draggingNode, {
+    properties: {
+        'background-color': '#EFCA72',
+        'box-shadow': '-2px 0px 5px #888888'
+    }
+})
+
+draggingNode.alignPos = new Align(draggingNode);
+draggingNode.gestures = new GestureHandler(draggingNode);
+draggingNode.gestures.on('drag', function(e, p){
+    var currentPos = draggingNode.getPosition()
+    var newPosX = currentPos[0] + e.centerDelta.x
+    if (newPosX < -100) {
+        console.log('hey there!!!');
+        draggingNode.alignPos.set(0.05, 0, 0, {duration: 1000, curve: 'easeOutBounce'})
+    }
+    draggingNode.setPosition(newPosX, currentPos[1])
 });
 
-// Let the magic begin...
-logo.requestUpdate(spinner);
+var item2 = draggingNode.addChild();
+item2
+    .setSizeMode('absolute', 'absolute')
+    .setAbsoluteSize(300, 300)
+    .setMountPoint(0.5, 0.5)
+    .setAlign(0.5, 0.5);
+
+item2.el = new DOMElement(item2, {
+    tagName: 'img',
+    properties: {
+        'background-color': '#E3F5BD',
+        'border': '5px solid',
+        'border-color': 'whitesmoke',
+        'border-radius': '20px',
+        'box-shadow': '7px 7px 10px #888888'
+    }
+});
+item2.el.setAttribute('src', '/images/ae.jpg');
+
+FamousEngine.init();
